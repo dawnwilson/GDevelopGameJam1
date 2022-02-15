7e5dc0a72c211951021e62beeadb51d39f135953
Dawn Wilson <16958583+dawnwilson@users.noreply.github.com>
2022-02-15 19:13:45 -0400

    Create Global to track times

---
+++ Src/Autoloads/Global.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var levelTimes : Dictionary = {1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0]}
+var minutes : int = 00 setget setMinutes, getMinutes
+var seconds : int = 00 setget setSeconds, getSeconds
+
+
+
+func setMinutes(num : int) -> void:
+	minutes += num
+
+
+func getMinutes() -> int:
+	return minutes
+
+
+func setSeconds(num : int) -> void:
+	seconds += num
+
+
+func getSeconds() -> int:
+	return seconds
+
+
+func submitFinishedLevelTime(lvl : int) -> void:
+	levelTimes[lvl][1][0] = getMinutes()
+	levelTimes[lvl][1][1] = getSeconds()
