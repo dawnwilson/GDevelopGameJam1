010af40117180a0175b8a3cd4b54afb45f2d33f3
Dawn Wilson <16958583+dawnwilson@users.noreply.github.com>
2022-02-15 16:48:42 -0400

    Tweak SceneChanger to use proper animation

--- Src/Autoloads/SceneChanger.gd
+++ Src/Autoloads/SceneChanger.gd
@@ -6,12 +6,14 @@ signal completeLevel()
 onready var animationPlayer = $AnimationPlayer
 
 
-func changeSceneBlack(path: PackedScene, delay = 0.5):
+
+func changeSceneBlack(path: PackedScene, delay = 0.35):
+	print("in change scene")
 	yield(get_tree().create_timer(delay), "timeout")
-	animationPlayer.play("FadeToBlack")
+	animationPlayer.play("Fade")
 	yield(animationPlayer, "animation_finished")
 	assert(get_tree().change_scene_to(path) == OK)
-	animationPlayer.play_backwards("FadeToBlack")
+	animationPlayer.play_backwards("Fade")
 	yield(animationPlayer, "animation_finished")
 	emit_signal("sceneChanged")
 
